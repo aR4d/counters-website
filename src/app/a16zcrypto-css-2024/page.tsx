589f2dc9d0040ac4c9bda8b15d336416b8343181
AlexandruRad <alexandru.rad@icloud.com>
2023-10-19 22:55:56 +0300

    routes and placeholders

---
+++ src/app/a16zcrypto-css-2024/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function a16zcryptoPage() {
+  return (
+    <div>
+      <h1>Here will go the a16z crypto token visual breakdown and video</h1>
+      <Image src="/placeholder-img.png" alt="token" width="300" height="500" />
+      <video src="/placeholder-video.mp4" width="750" height="500" controls></video>
+    </div>
+  );
+}

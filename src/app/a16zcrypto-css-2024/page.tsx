e1748e17a15963f3bac05fb0b52ff0297dc25797
AlexandruRad <alexandru.rad@icloud.com>
2023-10-20 18:58:30 +0300

    compressed image

--- src/app/a16zcrypto-css-2024/page.tsx
+++ src/app/a16zcrypto-css-2024/page.tsx
@@ -5,7 +5,7 @@ export default function a16zcryptoPage() {
     <div>
       <h1>Here will go the a16z crypto token visual breakdown and video</h1>
       <h2>Hoping to by some time since I found out about the Oct 20Th deadline only last week :(</h2>
-      <Image src="/counters-breakdown.png" alt="token" width="1000" height="1000" />
+      <Image src="/counters-breakdown-min.PNG" alt="token" width="1000" height="1000" />
       <video src="/placeholder-video.mp4" width="750" height="500" controls></video>
     </div>
   );
